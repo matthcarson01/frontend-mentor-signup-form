5518e256f9495ed1ba5655ef38ecc9b6f69c7f8a
matthcarson01 <matthcarson01@gmail.com>
2020-07-26 21:14:22 -0400

    updated marckup and css to show error state on input to match design

---
+++ index.js
@@ -0,0 +1,55 @@
+const form = document.getElementById('form');
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+var formSuccess=true;
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    checkTextInput(firstName);
+    checkTextInput(lastName);
+    checkEmailInput(email);
+    checkPasswordInput(password);
+
+    if(formSuccess){
+        return console.log('Submit Successful')
+    }else {
+        return console.log('Submit failed')
+    }
+
+})
+
+const checkTextInput = (input) => {
+    //check if value exist for text input
+    if(input.value.trim() === ''){
+        formSuccess = false;
+        return addErrorClass(input)
+    } else {
+        return 
+    }
+}
+
+const checkEmailInput = (input) => {
+    //check if value of email
+    if(input.value.trim() === ''){
+        formSuccess = false;
+        return addErrorClass(input)
+    } else {
+        return
+    }
+}
+
+const checkPasswordInput = (input) => {
+    //check if value of password
+    if(input.value.trim() === ''){
+        formSuccess = false;
+        return addErrorClass(input)
+    } else {
+        return
+    }
+}
+
+const addErrorClass = (targetElement) => {
+   return targetElement.parentElement.classList.add('error');
+}
